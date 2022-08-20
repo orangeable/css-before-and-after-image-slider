19ae3d164742dc9c4766cf8afada5b73e47bd107
nocoded <josh@nocoded.com>
2022-08-20 14:10:32 -0600

    Ability to add multiple sliders to a single page.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
     $("input.slider").on("input change", function(event) {
+        var element = $(this).parents("div.container");
         var pos = event.target.value;
 
-        $("div.before").css({width: pos + "%"});
-        $("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
+        element.find("div.before").css({width: pos + "%"});
+        element.find("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
     });
 
 });
